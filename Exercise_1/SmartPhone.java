a9f3b0fee915455d60be87ca66a1ca43781c066f
abbysetyo <abby.wiratama@gmail.com>
2016-02-02 21:28:08 +0700

    Update SmartPhone.java

--- Exercise_1/SmartPhone.java
+++ Exercise_1/SmartPhone.java
@@ -1,1 +1,40 @@
-
+//abbi setyo wiratama wicaksono
+//1301140321
+public class SmartPhone{
+		private Application[] appList =  new Application[100];
+		private int totalApp = 0;
+		private int memory;
+  	
+		public void setMemory(int memory){
+			this.memory = memory;
+  	}
+  
+		public void addApplication(AppStore appStore, int appId){
+			if(appStore.getApp(appId).getAppSize() < getRemainingSize()){
+				appList[totalApp] = appStore.getApp(appId); 
+				totalApp++;
+				System.out.println(appStore.getApp(appId).getAppName()+" Installed ");  		}
+ 		else
+ 		{
+ 			System.out.println("Memory Full !");
+ 		}
+  	}
+  	
+		public Application getApp(int id){
+			return appList[id]; 	}
+  	
+		public int getTotalApp(){
+			return totalApp;  	}
+  	
+		public int getRemainingSize(){
+			int x = 0;
+			if(getTotalApp() > 0){
+				for(int a = 0; a < getTotalApp(); a++){
+					x += appList[a].getAppSize(); 			}
+  		}
+  		return memory - x;
+  	}
+  	
+		public String toString(){
+			return "Memory Size " + getRemainingSize() + ", " + getTotalApp() + " application installed, remaining memory size : " +  getRemainingSize() + "MB";}
+  }
