f7df399bb87a8172e5a7da43350f9a2bbfc05c12
abbysetyo <abby.wiratama@gmail.com>
2016-02-02 21:22:22 +0700

    Update AppStore.java

--- Exercise_1/AppStore.java
+++ Exercise_1/AppStore.java
@@ -1,1 +1,25 @@
+//abbi setyo wiratama wicaksono
+//1301140321
 
+public class AppStore{
+ 	private Application[] appList = new Application[100];
+		private  int totalApp = 0;
+  	
+		public void createNewApp(String appName, int appSize) {
+			Application newApp = new Application(appName, appSize);
+			appList[totalApp] = newApp;
+			totalApp++;  	}
+ 	
+		public int getTotalApp(){
+			return totalApp; 	}
+   
+		public Application getApp(int id){
+			return appList[id];	}
+  	
+		public Application[] getAppList(){
+			return appList; 	}
+  	 
+		public String toString(){
+			return "There are " + totalApp + " applications ready to install";  	}
+  }
+//
