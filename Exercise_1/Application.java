1f29cc0ad27c2cd5ea27c0e26e4bb33505c45fb7
abbysetyo <abby.wiratama@gmail.com>
2016-02-02 21:25:53 +0700

    Update Application.java

--- Exercise_1/Application.java
+++ Exercise_1/Application.java
@@ -1,1 +1,20 @@
-
+	//abbi setyo wiratama wicaksono
+	//1301140321
+	
+	public class Application{
+		private String appName;
+		private int appSize;
+  	
+		public Application(String appName, int appSize){
+			this.appName = appName;
+			this.appSize = appSize;  	}
+  	
+		public String getAppName(){
+			return appName;	}
+ 	
+		public int getAppSize(){
+			return appSize; 	}
+  	
+		public String toString(){
+			return "Application Name : " + appName + ", with size : " + appSize + " MB";  	}
+  }
