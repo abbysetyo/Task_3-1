19c04b0631e4ec2bd129032db0c31c0ba6426f04
abbysetyo <abby.wiratama@gmail.com>
2016-02-02 21:27:21 +0700

    Update Driver.java

--- Exercise_1/Driver.java
+++ Exercise_1/Driver.java
@@ -1,1 +1,20 @@
-
+//abbi setyo wiratama wicaksono
+//1301140321
+	public class Driver{
+		public static void main(String[] args) {
+			AppStore aps = new AppStore();
+			aps.createNewApp("app1", 100);
+			aps.createNewApp("app2", 200);
+			aps.createNewApp("app3", 300);
+			aps.createNewApp("app4", 400);
+			System.out.println(aps.toString());
+			System.out.println(aps.getApp(1).toString());
+			System.out.println(aps.getApp(2).toString());
+			System.out.println(aps.getApp(3).toString());
+			SmartPhone sm = new SmartPhone();
+			sm.setMemory(300);
+			System.out.println(sm.toString());
+			sm.addApplication( aps, 0 );
+			sm.addApplication( aps, 1 );
+			System.out.println(sm.toString());     }
+  }
