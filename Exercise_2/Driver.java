7d6f01b253bca75da83375fd6406384fc19d500c
abbysetyo <abby.wiratama@gmail.com>
2016-02-02 21:29:00 +0700

    Update Driver.java

--- Exercise_2/Driver.java
+++ Exercise_2/Driver.java
@@ -1,1 +1,39 @@
-
+//abbi setyo wiratama wicaksono
+//1301140321
+public class Driver
+  {
+  	public static void main(String[]args)
+  	{
+  		StartUp stp = new StartUp();
+  		Member a = new Member("Alex");
+ 		Member b = new Member("Beni");
+ 		Member c = new Member("Cakra");
+ 		Member d = new Member("Deni");
+ 		Member e = new Member("Eric");
+  		stp.addMember(a);
+  		stp.addMember(b);
+  		stp.addMember(c);
+ 		stp.addMember(d);
+  		stp.addMember(e);
+  		stp.createNewProject( "project1" );
+  		stp.createNewProject( "project2" );
+  		Project p1 = stp.getProject( 0 );
+  		Project p2 = stp.getProject( 1 );
+  		for(int i = 0; i < 3; i++)
+  		{
+ 			Member m = stp.getMember( i );
+  			stp.setProjectMember( p1 , m );
+ 		}
+  		for(int i = 2; i < 5; i++)
+  		{
+  			Member m = stp.getMember( i );
+  			stp.setProjectMember( p2 , m );
+  		}
+ 		stp.releaseProject( stp.getProject( 0 ) );
+  		for(int i = 0; i < 5; i++)
+  			System.out.println( stp.getMember( i ).toString() );
+ 		System.out.println( stp.getProject( 0 ).toString() );
+  		System.out.println( stp.getProject( 1 ).toString() );
+ 	}
+  	
+  }
