f8c48f35719d6886a34c8549e4f11be7c2f32200
abbysetyo <abby.wiratama@gmail.com>
2016-02-02 21:30:14 +0700

    Update Project.java

--- Exercise_2/Project.java
+++ Exercise_2/Project.java
@@ -1,1 +1,37 @@
-
+//abbi setyo wiratama wicaksono
+1301140321
+public class Project {
+ 	private String projectName;
+ 	private Member[] teamMember = new Member[5];
+ 	private int nTeam = 0;
+ 	private boolean releaseStatus = false;
+ 	
+ 	public Project(String projectName){
+ 		this.projectName = projectName;
+ 		teamMember = new Member[5];
+ 		releaseStatus = false;
+ 	}
+ 	
+ 	
+ 	public boolean isReleased(){
+ 		return releaseStatus;
+ 	}
+ 	
+ 	public void releaseApp(){
+ 		releaseStatus = true;
+ 	}
+ 	
+ 	public void addMember(Member m){
+ 		if(nTeam<teamMember.length){
+ 			teamMember[nTeam] = m;
+ 			nTeam++;
+ 		}
+ 		else{
+ 			System.out.println("full array");
+ 		}
+ 	}
+ 	
+ 	public String toString(){
+ 		return "Project "+projectName+" status is in progress with team member of "+nTeam;
+ 	}
+ }
