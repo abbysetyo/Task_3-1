015d79bc53221f44b7732fb3d743e3ba7f17da39
abbysetyo <abby.wiratama@gmail.com>
2016-02-02 21:29:42 +0700

    Update Member.java

--- Exercise_2/Member.java
+++ Exercise_2/Member.java
@@ -1,1 +1,45 @@
-
+//abbi setyo wiratama wicaksono
+//1301140321
+public class Member {
+ 	private String name;
+ 	private String specialization;
+ 	private int projectWorked = 0;
+ 
+ 
+ public Member (String name) {
+ 	this.name = name;
+ }
+ 
+ public Member (String name, String specialization) {
+ 	this.name = name;
+ 	this.specialization = specialization;
+ }
+ 
+ public String getName() {
+ 	return name;
+ }
+ 
+ public void setName(String name) {
+     this.name = name;
+ }
+ 
+ public String getspecialization() {
+     return specialization;
+ }
+ 
+ public void setSpecialization(String specialization) {
+     this.specialization = specialization;
+ }
+ 
+ public int getProjectWorked() {
+     return projectWorked;
+ }
+ 
+ public void setProjectWorked(int projectWorked) {
+     this.projectWorked = projectWorked;
+ }
+ 	
+ ublic String toString(){
+ 	return "Member "+name+" specialized in "+specialization+" and already worked on "+projectWorked+" project";
+ 	}
+ }
